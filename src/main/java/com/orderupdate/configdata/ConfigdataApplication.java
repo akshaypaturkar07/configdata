43a24329afb295e61ddae639adc96006069d830d
Akshay Paturkar <akshay.paturkar@jda.com>
2020-06-24 20:17:17 +0530

    initial commit

---
+++ src/main/java/com/orderupdate/configdata/ConfigdataApplication.java
@@ -0,0 +1,13 @@
+package com.orderupdate.configdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigdataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orderupdate/configdata/ConfigdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderupdate.configdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
